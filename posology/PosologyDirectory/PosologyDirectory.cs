efd9158b0a3db83db653cf44fdbe5194e33b93c8
Halima Koundi <hkoundi@gmail.com>
2020-04-12 20:22:12 +0200

    delete old function

--- posology/PosologyDirectory/PosologyDirectory.cs
+++ posology/PosologyDirectory/PosologyDirectory.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Posology.Core;
 
 namespace PosologyDirectory
@@ -15,7 +12,7 @@ namespace PosologyDirectory
     {
         [FunctionName("PosologyDirectory")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "posologydirectory/barcode/{code}")] HttpRequest req, string code,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "directory/barcode/{code}")] HttpRequest req, string code,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
