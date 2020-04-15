5deb592c0e0b9a7ad415ce8e91ed4ad013060cbf
Halima Koundi <hkoundi@gmail.com>
2020-04-15 19:01:33 +0200

    move classes to sub foldersfor us data model

---
+++ posology/Posology.Directory/Medication/UnitedStates/USDrug.cs
@@ -0,0 +1,14 @@
+﻿using Posology.Core;
+
+namespace Posology.Directory.Medication.UnitedStates
+{
+    public class USDrug : IDrug
+    {
+        public string InternalIdentifier { get; set; }
+        public string Denomination { get; set; }
+        public string AutorisationStatus { get; set; }
+        public string AdministrationType { get; set; }
+        public string UnkownNumber { get; set; }
+        public string DrugType { get; set; }
+    }
+}
---
+++ posology/Posology.Directory/Medication/UnitedStates/USDrugComponent.cs
@@ -0,0 +1,21 @@
+﻿using Posology.Core;
+
+namespace Posology.Directory.Medication.UnitedStates
+{
+    public class USDrugComponent:IDrugComponent
+    {
+        public USDrugComponent() { }
+        public USDrugComponent(string internalDrugIdentifier)
+        {
+            InternalDrugIdentifier = internalDrugIdentifier;
+        }
+
+        public string InternalDrugIdentifier { get; set; }
+        public string DrugShape { get; set; }
+        public string ComponentId { get; set; }
+        public string ComponentName { get; set; }
+        public string ComponentAmount { get; set; }
+        public string ComponentAmountUnit { get; set; }
+        public string ComponentType { get; set; }
+    }
+}
